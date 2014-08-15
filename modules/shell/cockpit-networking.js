b2130549bc4f95666c63c62ba1b4aa32d4cf8e78
Marius Vollmer <mvollmer@redhat.com>
2014-08-15 15:12:31 +0300

    shell: List all interesting network interfaces
    except the interface being configured, when configuring children or
    parents.

--- modules/shell/cockpit-networking.js
+++ modules/shell/cockpit-networking.js
@@ -2170,6 +2170,18 @@ function PageNetworkIpSettings() {
 
 cockpit.pages.push(new PageNetworkIpSettings());
 
+function is_interface_connection(iface, connection) {
+    return connection && connection.Interfaces.indexOf(iface) != -1;
+}
+
+function is_interesting_interface(iface) {
+    return (!iface.Device ||
+            iface.Device.DeviceType == 1 ||
+            iface.Device.DeviceType == 10 ||
+            iface.Device.DeviceType == 11 ||
+            iface.Device.DeviceType == 13);
+}
+
 PageNetworkBondSettings.prototype = {
     _init: function () {
         this.id = "network-bond-settings-dialog";
@@ -2267,7 +2279,8 @@ PageNetworkBondSettings.prototype = {
                     $('<td>').text(_("Members")),
                     $('<td>').append(
                         model.list_interfaces().map(function (iface) {
-                            if (!iface.Device || iface.Device.DeviceType != 1)
+                            if (is_interface_connection(iface, PageNetworkBondSettings.connection) ||
+                                !is_interesting_interface(iface))
                                 return null;
                             return $('<label>').append(
                                 $('<input>', { 'type': "checkbox",
@@ -2493,7 +2506,8 @@ PageNetworkBridgeSettings.prototype = {
                     $('<td>').text(_("Members")),
                     $('<td>').append(
                         model.list_interfaces().map(function (iface) {
-                            if (!iface.Device || iface.Device.DeviceType != 1)
+                            if (is_interface_connection(iface, PageNetworkBridgeSettings.connection) ||
+                                !is_interesting_interface(iface))
                                 return null;
                             return $('<label>').append(
                                 $('<input>', { 'type': "checkbox",
@@ -2788,10 +2802,8 @@ PageNetworkVlanSettings.prototype = {
 
         var parent_choices = [];
         model.list_interfaces().forEach(function (i) {
-            if (!i.Device ||
-                i.Device.DeviceType == 1 ||
-                i.Device.DeviceType == 10 ||
-                i.Device.DeviceType == 13)
+            if (!is_interface_connection(i, PageNetworkVlanSettings.connection) &&
+                is_interesting_interface(i))
                 parent_choices.push({ title: i.Name, choice: i.Name });
         });
 
